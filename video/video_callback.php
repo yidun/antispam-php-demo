aa5a580be212ba5d5c2bd2a46e13b78696c338b8
杨卫强 <yangweiqiang@corp.netease.com>
2016-08-25 14:49:54 +0800

    调整备注

--- video/video_callback.php
+++ video/video_callback.php
@@ -6,7 +6,7 @@ define("SECRETID", "your_secret_id");
 define("SECRETKEY", "your_secret_key");
 /** 业务ID，易盾根据产品业务特点分配 */
 define("BUSINESSID", "your_business_id");
-/** 易盾反垃圾云服务直播视频检测结果获取接口地址 */
+/** 易盾反垃圾云服务视频检测结果获取接口地址 */
 define("API_URL", "https://api.aq.163.com/v2/video/callback/results");
 /** api version */
 define("VERSION", "v2");
