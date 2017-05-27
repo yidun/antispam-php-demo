98c9f94319efc71e73535bc820b28d0ea017e881
胡敏 <hzhumin1@corp.netease.com>
2017-05-27 19:57:49 +0800

    点播check升级为v3,点播taskId查询接口

--- video/video_query.php
+++ video/video_query.php
@@ -1,17 +1,16 @@
 <?php
-/** 视频提交检测接口 */
 /** 产品密钥ID，产品标识 */
 define("SECRETID", "your_secret_id");
 /** 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露 */
 define("SECRETKEY", "your_secret_key");
 /** 业务ID，易盾根据产品业务特点分配 */
 define("BUSINESSID", "your_business_id");
-/** 易盾反垃圾云服务视频检测接口地址 */
-define("API_URL", "https://api.aq.163.com/v3/video/submit");
+/** 易盾反垃圾云服务点播結果查詢接口地址 */
+define("API_URL", "https://api.aq.163.com/v1/video/query/task");
 /** api version */
-define("VERSION", "v3");
+define("VERSION", "v1");
 /** API timeout*/
-define("API_TIMEOUT", 1);
+define("API_TIMEOUT", 10);
 /** php内部使用的字符串编码 */
 define("INTERNAL_STRING_CHARSET", "auto");
 
@@ -80,27 +79,37 @@ function check($params){
 // 简单测试
 function main(){
     echo "mb_internal_encoding=".mb_internal_encoding()."\n";
+	$taskIds = array("a42d1727d40f47cba8de3458820cb814","01fb38b09d694dbaae0560e62c1fc2af");
 	$params = array(
-		"dataId"=>"fbfcad1c-dba1-490c-b4de-e784c2691765",
-		"url"=>"http://xxx.xxx.com/xxxx",
-		"callback"=>"{\"p\":\"xx\"}",
+		"taskIds"=>json_encode($taskIds)
 	);
 
 	$ret = check($params);
-	var_dump($ret);
+	//var_dump($ret);
 	if ($ret["code"] == 200) {
-		$result = $ret["result"];
-        // status 0:成功，1:失败
-        $status = $result["status"];
-        $taskId = $result["taskId"];
-        if($status === 0){
-            echo "提交成功，taskId=".$taskId;
-        }else{
-            echo "提交失败，taskId=".$taskId;
-        }
-    }else{
-    	var_dump($ret);
-    }
+		$result_array = $ret["result"];
+		foreach($result_array as $res_index => $result){
+			//0:正常，10：检测中，20：不是7天内数据，30：taskId不存在，110：请求重复，120：参数错误，130：解析错误，140：数据类型错误
+		    $status = $result["status"];
+		    $taskId = $result["taskId"];
+			if($status!=0){
+				echo "获取结果异常，status=".$status."taskId=".$taskId;
+				continue;
+			}
+			$callback = $result["callback"];
+			$level = $result["level"];
+			if($level != 0){ // 返回 level == 0表示正常
+				// 从evidences里获取证据信息，详细说明见http://dun.163.com/support/api#API_13
+				foreach ($result['evidences'] as $evi_index => $evidence) {
+					echo json_encode($evidence)."taskId=".$taskId;
+				}
+			} else {
+				echo "点播视频结果正常，callback=".$callback."taskId=".$taskId;
+			}
+		}
+    	}else{
+    		var_dump($ret); // error handler
+    	}
 }
 
 main();
