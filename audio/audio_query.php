7e36af80295ed29eee7a92c906d2c3e56ce62bd7
hzhumin1 <hzhumin1@corp.netease.com>
2020-04-23 15:16:39 +0800

    php demo http请求统一优化成curl方式

--- audio/audio_query.php
+++ audio/audio_query.php
@@ -7,44 +7,12 @@ define("SECRETKEY", "your_secret_key");
 /** 业务ID，易盾根据产品业务特点分配 */
 define("BUSINESSID", "your_business_id");
 /** 易盾反垃圾云服务音频taskId查询接口地址 */
-define("API_URL", "https://as.dun.163yun.com/v1/audio/query/task");
+define("API_URL", "https://as.dun.163yun.com/v3/audio/query/task");
 /** api version */
-define("VERSION", "v1");
+define("VERSION", "v3");
 /** API timeout*/
 define("API_TIMEOUT", 10);
-/** php内部使用的字符串编码 */
-define("INTERNAL_STRING_CHARSET", "auto");
-
-
-/**
- * 计算参数签名
- * $params 请求参数
- * $secretKey secretKey
- */
-function gen_signature($secretKey, $params){
-	ksort($params);
-	$buff="";
-	foreach($params as $key=>$value){
-	     if($value !== null) {
-	        $buff .=$key;
-		$buff .=$value;
-    	     }
-	}
-	$buff .= $secretKey;
-	return md5($buff);
-}
-
-/**
- * 将输入数据的编码统一转换成utf8
- * @params 输入的参数
- */
-function toUtf8($params){
-	$utf8s = array();
-    foreach ($params as $key => $value) {
-    	$utf8s[$key] = is_string($value) ? mb_convert_encoding($value, "utf8", INTERNAL_STRING_CHARSET) : $value;
-    }
-    return $utf8s;
-}
+require("../util.php");
 
 /**
  * 反垃圾请求接口简单封装
@@ -61,16 +29,7 @@ function check($params){
 	$params["signature"] = gen_signature(SECRETKEY, $params);
 	// var_dump($params);
 
-	$options = array(
-	    "http" => array(
-	        "header"  => "Content-type: application/x-www-form-urlencoded\r\n",
-	        "method"  => "POST",
-	        "timeout" => API_TIMEOUT, // read timeout in seconds
-	        "content" => http_build_query($params),
-	    ),
-	);
-	$context  = stream_context_create($options);
-	$result = file_get_contents(API_URL, false, $context);
+	$result = curl_post($params, API_URL, API_TIMEOUT);
 	// var_dump($result);
 	if($result === FALSE){
 		return array("code"=>500, "msg"=>"file_get_contents failed.");
@@ -91,19 +50,12 @@ function main(){
 	$ret = check($params);
 	var_dump($ret);
 	if ($ret["code"] == 200) {
-		$result = $ret["result"];
-		// var_dump($array);
-		foreach($result as $index => $audio_ret){
-		    $taskId = $audio_ret["taskId"];
-		    $action= $audio_ret["action"];
-		    // 分类信息
-		    $labelArray = $audio_ret["labels"];
-		    if($action==0){
-			    echo "结果：通过,taskId:{$taskId}\n";
-		    }else if($action==2){
-			    echo "结果：不通过,taskId:{$taskId}\n";
-		    }
-		}
+	    // 反垃圾结果
+		$antispamArray = $ret["antispam"];
+		// 语种结果
+        $languageArray = $ret["language"];
+        // 语音翻译结果
+        $asrArray = $ret["asr"];
     }else{
     	var_dump($ret);
     }
